c13416b15ed96adfae9f099f5e2c5d013d4c96fb
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-06 16:33:37 +0300

    navbar added

--- ecommfrontend/src/App.vue
+++ ecommfrontend/src/App.vue
@@ -1,5 +1,18 @@
 <template>
+<div class="app">
+  <Navbar/>  
+  <router-view/>
+</div>
 
+  
 </template>
 
+<script>
+import Navbar from './components/Navbar'
+export default {
+  components:{
+    Navbar,
+  }
+}
+</script>
 
