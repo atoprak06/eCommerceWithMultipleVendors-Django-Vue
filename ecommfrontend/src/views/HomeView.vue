92f1c904d2c3bcf56a058aee473099aed8350eae
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-06 17:07:36 +0300

    fake product frontpage created

--- ecommfrontend/src/views/HomeView.vue
+++ ecommfrontend/src/views/HomeView.vue
@@ -1,5 +1,139 @@
 <template>
   <div class="home">
+    <div class="container-fluid bg-dark">
+      <h3 class="text-warning text-center">New Products</h3>
+      <div class="p-5 row d-flex">
+        <div class=" my-3 col-lg-3 col-md-4 col-sm-6">
+          <div class="card p-1" style="width: 18rem;">
+            <img src="../assets/logo.png" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title d-flex justify-content-between">
+                <span class="text-dark">Product Name</span>
+                <span class="text-danger">$10.00</span>
+              </h5>              
+              <a href="/" class="text-decoration-none"><small class="small bg-dark text-white rounded-3 p-1">Product Category</small> </a>             
+              <p class="card-text mt-3">Product desc. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sunt.</p>
+              <a href="#" class="btn btn-primary">Go to Product</a>
+              <hr>
+              <a href="/" class="text-decoration-none"><p class="small rounded-3 text-dark fw-bold text-center p-1 m-0">Vendor Name</p></a>
+            </div>
+          </div>
+        </div>
+        <div class=" my-3 col-lg-3 col-md-4 col-sm-6">
+          <div class="card p-1" style="width: 18rem;">
+            <img src="../assets/logo.png" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title d-flex justify-content-between">
+                <span class="text-dark">Product Name</span>
+                <span class="text-danger">$10.00</span>
+              </h5>              
+              <a href="/" class="text-decoration-none"><small class="small bg-dark text-white rounded-3 p-1">Product Category</small> </a>             
+              <p class="card-text mt-3">Product desc. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sunt.</p>
+              <a href="#" class="btn btn-primary">Go to Product</a>
+              <hr>
+              <a href="/" class="text-decoration-none"><p class="small rounded-3 text-dark fw-bold text-center p-1 m-0">Vendor Name</p></a>
+            </div>
+          </div>
+        </div>
+        <div class=" my-3 col-lg-3 col-md-4 col-sm-6">
+          <div class="card p-1" style="width: 18rem;">
+            <img src="../assets/logo.png" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title d-flex justify-content-between">
+                <span class="text-dark">Product Name</span>
+                <span class="text-danger">$10.00</span>
+              </h5>              
+              <a href="/" class="text-decoration-none"><small class="small bg-dark text-white rounded-3 p-1">Product Category</small> </a>             
+              <p class="card-text mt-3">Product desc. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sunt.</p>
+              <a href="#" class="btn btn-primary">Go to Product</a>
+              <hr>
+              <a href="/" class="text-decoration-none"><p class="small rounded-3 text-dark fw-bold text-center p-1 m-0">Vendor Name</p></a>
+            </div>
+          </div>
+        </div>
+        <div class=" my-3 col-lg-3 col-md-4 col-sm-6">
+          <div class="card p-1" style="width: 18rem;">
+            <img src="../assets/logo.png" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title d-flex justify-content-between">
+                <span class="text-dark">Product Name</span>
+                <span class="text-danger">$10.00</span>
+              </h5>              
+              <a href="/" class="text-decoration-none"><small class="small bg-dark text-white rounded-3 p-1">Product Category</small> </a>             
+              <p class="card-text mt-3">Product desc. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sunt.</p>
+              <a href="#" class="btn btn-primary">Go to Product</a>
+              <hr>
+              <a href="/" class="text-decoration-none"><p class="small rounded-3 text-dark fw-bold text-center p-1 m-0">Vendor Name</p></a>
+            </div>
+          </div>
+        </div>
+        <div class=" my-3 col-lg-3 col-md-4 col-sm-6">
+          <div class="card p-1" style="width: 18rem;">
+            <img src="../assets/logo.png" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title d-flex justify-content-between">
+                <span class="text-dark">Product Name</span>
+                <span class="text-danger">$10.00</span>
+              </h5>              
+              <a href="/" class="text-decoration-none"><small class="small bg-dark text-white rounded-3 p-1">Product Category</small> </a>             
+              <p class="card-text mt-3">Product desc. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sunt.</p>
+              <a href="#" class="btn btn-primary">Go to Product</a>
+              <hr>
+              <a href="/" class="text-decoration-none"><p class="small rounded-3 text-dark fw-bold text-center p-1 m-0">Vendor Name</p></a>
+            </div>
+          </div>
+        </div>
+        <div class=" my-3 col-lg-3 col-md-4 col-sm-6">
+          <div class="card p-1" style="width: 18rem;">
+            <img src="../assets/logo.png" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title d-flex justify-content-between">
+                <span class="text-dark">Product Name</span>
+                <span class="text-danger">$10.00</span>
+              </h5>              
+              <a href="/" class="text-decoration-none"><small class="small bg-dark text-white rounded-3 p-1">Product Category</small> </a>             
+              <p class="card-text mt-3">Product desc. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sunt.</p>
+              <a href="#" class="btn btn-primary">Go to Product</a>
+              <hr>
+              <a href="/" class="text-decoration-none"><p class="small rounded-3 text-dark fw-bold text-center p-1 m-0">Vendor Name</p></a>
+            </div>
+          </div>
+        </div>
+        <div class=" my-3 col-lg-3 col-md-4 col-sm-6">
+          <div class="card p-1" style="width: 18rem;">
+            <img src="../assets/logo.png" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title d-flex justify-content-between">
+                <span class="text-dark">Product Name</span>
+                <span class="text-danger">$10.00</span>
+              </h5>              
+              <a href="/" class="text-decoration-none"><small class="small bg-dark text-white rounded-3 p-1">Product Category</small> </a>             
+              <p class="card-text mt-3">Product desc. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sunt.</p>
+              <a href="#" class="btn btn-primary">Go to Product</a>
+              <hr>
+              <a href="/" class="text-decoration-none"><p class="small rounded-3 text-dark fw-bold text-center p-1 m-0">Vendor Name</p></a>
+            </div>
+          </div>
+        </div>
+        <div class=" my-3 col-lg-3 col-md-4 col-sm-6">
+          <div class="card p-1" style="width: 18rem;">
+            <img src="../assets/logo.png" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title d-flex justify-content-between">
+                <span class="text-dark">Product Name</span>
+                <span class="text-danger">$10.00</span>
+              </h5>              
+              <a href="/" class="text-decoration-none"><small class="small bg-dark text-white rounded-3 p-1">Product Category</small> </a>             
+              <p class="card-text mt-3">Product desc. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sunt.</p>
+              <a href="#" class="btn btn-primary">Go to Product</a>
+              <hr>
+              <a href="/" class="text-decoration-none"><p class="small rounded-3 text-dark fw-bold text-center p-1 m-0">Vendor Name</p></a>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
     
   </div>
 </template>
