f7af424aabc101d40b9666b2f21af098bad5a3b5
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-07 01:05:38 +0300

    axios installed for requests

--- ecommfrontend/src/main.js
+++ ecommfrontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap' 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
 
+axios.defaults.baseURL='http://127.0.0.1:8000'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router,axios).mount('#app')
--- ecommfrontend/src/views/RegisterView.vue
+++ ecommfrontend/src/views/RegisterView.vue
@@ -2,22 +2,22 @@
     <div class="register">
         <div class="container bg-dark d-flex flex-column align-items-center mt-5">
             <h3 class="text-white text-center">Register</h3>            
-            <form class="mt-5" style="width:100%; max-width:30rem"> 
+            <form @onSubmit="submit" class="mt-5" style="width:100%; max-width:30rem"> 
                 <div class="mb-3">
                     <label for="username" class="form-label text-white">Username:</label>
-                    <input type="text" class="form-control" id="username" aria-describedby="emailHelp">                    
+                    <input type="text" v-model="username" class="form-control" id="username" aria-describedby="emailHelp">                    
                 </div>
                 <div class="mb-3">
                     <label for="mail" class="form-label text-white">Email:</label>
-                    <input type="mail" class="form-control" id="mail" aria-describedby="emailHelp">                    
+                    <input type="mail" v-model="email" class="form-control" id="mail" aria-describedby="emailHelp">                    
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label text-white">Password:</label>
-                    <input type="password" class="form-control" id="password">
+                    <input type="password" v-model="password" class="form-control" id="password">
                 </div>
                 <div class="mb-3">
                     <label for="password2" class="form-label text-white">Confirm Password:</label>
-                    <input type="password2" class="form-control" id="password">
+                    <input type="password2" v-model="re_password" class="form-control" id="password">
                 </div>
                 
                 <div class="mb-3 text-end">
@@ -34,6 +34,26 @@
 
 <script>
 export default {
-    name:'SignInView'
+    name:'SignInView',
+    data(){
+        return {
+            username:'',
+            email:'',
+            password:'',
+            re_password:''
+        }
+    },
+    methods:{
+        submit(){
+            const user={
+                username :this.username,
+                email: this.email,
+                password : this.password,
+                re_password:this.re_password
+            }
+            
+        }
+
+    }
 }
 </script>
