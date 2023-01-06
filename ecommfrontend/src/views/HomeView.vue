b508246027b200b1bfaf7ddb5410ede0831609b9
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-06 17:26:10 +0300

    homeview style corrected

--- ecommfrontend/src/views/HomeView.vue
+++ ecommfrontend/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div class="container-fluid bg-dark">
-      <h3 class="text-warning text-center">New Products</h3>
+      <h3 class="text-warning text-center mt-3">New Products</h3>
       <div class="p-5 row d-flex">
         <div v-for="product in 8" :key="product" class="my-3 col-lg-3 col-md-4 col-sm-6">
           <Product/>
