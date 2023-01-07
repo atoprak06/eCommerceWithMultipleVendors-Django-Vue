1caa5c235488fec144cf005cc774ce25f6a07e07
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-07 16:56:07 +0300

    custom user model created and djoser settings modified

--- ecommbackend/ecommbackend/settings.py
+++ ecommbackend/ecommbackend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+    'user'
 
 ]
 
@@ -58,6 +59,9 @@ MIDDLEWARE = [
 
 DJOSER = {
     'USER_CREATE_PASSWORD_RETYPE' : True,
+    'SERIALIZERS': {
+         'user_create': 'user.serializer.UserRegistrationSerializer'
+    }
 }
 
 ROOT_URLCONF = 'ecommbackend.urls'
@@ -132,3 +136,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.UserProfile'
--- ecommbackend/user/models.py
+++ ecommbackend/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class UserProfile(AbstractUser):
+    created_at = models.DateTimeField(auto_now_add=True)   
+    user_country = models.CharField('Country',max_length=100,null=True,blank=True)
+    user_city=models.CharField('City',max_length=100,null=True,blank=True)
+    user_address=models.CharField('address',max_length=255,null=True,blank=True)
+    gender_choices=(('male','Male'),('female','Female'),('other','Other'))
+    user_gender=models.CharField('Gender',max_length=20,choices=gender_choices,default='male')
+    is_vendor = models.BooleanField('Vendor',default=False)
+    user_age = models.PositiveIntegerField('Age',validators=[MinValueValidator(18), MaxValueValidator(100)],null=True,blank=True)    
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.username
