d6ac04386b99dc08e2adfd8260893c29529d7f62
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-08 19:22:41 +0300

    pinia setActivePinia setting added

--- ecommfrontend/src/main.js
+++ ecommfrontend/src/main.js
@@ -1,13 +1,18 @@
+import { createPinia, setActivePinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap' 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
-import { createPinia } from 'pinia'
+
 
 axios.defaults.baseURL='http://127.0.0.1:8000'
 
 const pinia = createPinia()
+setActivePinia(pinia)
+const app = createApp(App)
 
-createApp(App).use(router,axios,pinia).mount('#app')
+app.use(router,pinia,axios)
+
+app.mount('#app')
