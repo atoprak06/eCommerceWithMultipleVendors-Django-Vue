12e295e3a2ded95175ba2f8e37a7a58240d771cf
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-10 17:10:37 +0300

    category and product router creaded and included to url path

--- ecommbackend/products/urls.py
+++ ecommbackend/products/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register('products',views.ProductViewSet,basename='products')
+router.register('categories',views.CategoryViewSet,basename='categories')
+
+
 urlpatterns=[
-    path('',views.products)
+    path('',include(router.urls)),   
 ]
