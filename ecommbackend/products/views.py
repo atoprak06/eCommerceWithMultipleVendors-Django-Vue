1ef549868d72bdc702e9a30e771d98358a6a1b7e
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-10 17:11:22 +0300

    product and category viewset created

--- ecommbackend/products/views.py
+++ ecommbackend/products/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from .serializers import ProductSerializer,CategorySerializer
+from .models import Product,Category
 
-# Create your views here.
-@api_view(('GET',))
-def products(request):
-    return Response('this is product page')
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+    @action(methods=['get','patch',],permission_classes=[IsAuthenticatedOrReadOnly],detail=True)
+    def get_product(self,request,pk=None):
+        product = Product.objects.get(id=pk)
+        if product.created_by == request.user:
+            request.data['id'] = pk
+            Product.objects.filter(id=pk).update(**request.data)
+            product =  Product.objects.get(id=pk)                
+            serializer = ProductSerializer(product,many=False)
+            return Response(serializer.data)
+        else:
+            return Response('product not found or you are not authorized')
+
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
