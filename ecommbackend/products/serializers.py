8691a3cbd6023ed3af9eb03290187c72206e2e06
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-11 15:32:14 +0300

    code refactored

--- ecommbackend/products/serializers.py
+++ ecommbackend/products/serializers.py
@@ -2,12 +2,21 @@ from rest_framework import serializers
 from .models import Category,Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(queryset=Category.objects.all(),slug_field='title')
+    created_by = serializers.ReadOnlyField(source='created_by.username')
     class Meta:
         model= Product        
         fields = '__all__'
-        read_only_fields = ['id']
+        read_only_fields = ['id','created_by']
+
+    # def to_representation(self, instance):
+    #     rep = super(ProductSerializer, self).to_representation(instance)
+    #     rep['category'] = instance.category.title
+    #     rep['created_by'] = instance.created_by.username
+    #     return rep
 
 class CategorySerializer(serializers.ModelSerializer):
+    products = serializers.SlugRelatedField(many=True,queryset=Product.objects.all(),slug_field='title')
     class Meta:
         model= Category
-        fields = '__all__'
+        fields = ('title','slug','products')
--- ecommbackend/products/views.py
+++ ecommbackend/products/views.py
@@ -1,30 +1,36 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 from rest_framework.response import Response
 from .serializers import ProductSerializer,CategorySerializer
 from .models import Product,Category
+from user.models import UserProfile
+from user.serializers import UserShowSerializer
 
 
-class ProductViewSet(viewsets.ReadOnlyModelViewSet):
-
+class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]  
 
-    @action(methods=['get','patch',],permission_classes=[IsAuthenticatedOrReadOnly],detail=True)
-    def get_product(self,request,pk=None):
-        product = Product.objects.get(id=pk)
-        if product.created_by == request.user:
-            request.data['id'] = pk
-            Product.objects.filter(id=pk).update(**request.data)
-            product =  Product.objects.get(id=pk)                
-            serializer = ProductSerializer(product,many=False)
-            return Response(serializer.data)
-        else:
-            return Response('product not found or you are not authorized')
+    def perform_create(self, serializer):        
+        serializer.save(created_by=self.request.user)
+    
+    @action(detail=False)
+    def my_products(self,request):
+        products = Product.objects.filter(created_by=request.user)
+        serializer= ProductSerializer(products,many=True)
+        return Response(serializer.data)
 
-
+class VendorsViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = UserShowSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+    
+    def get_queryset(self):
+        return UserProfile.objects.filter(is_vendor=True) 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
