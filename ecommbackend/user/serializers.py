675d6f64a5b5fcbecd2ca74440803e2b1bf34f67
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-11 15:35:55 +0300

    product relation added

--- ecommbackend/user/serializers.py
+++ ecommbackend/user/serializers.py
@@ -1,6 +1,8 @@
 from djoser.serializers import UserCreatePasswordRetypeSerializer
 from djoser.serializers import UserSerializer
+from rest_framework import serializers
 from .models import UserProfile
+from products.models import Product
 
 class UserRegistrationSerializer(UserCreatePasswordRetypeSerializer):
     class Meta:
@@ -8,6 +10,7 @@ class UserRegistrationSerializer(UserCreatePasswordRetypeSerializer):
         fields = ('username','email','first_name','last_name','user_age','user_country','user_city','user_address','is_vendor','user_gender','password')
 
 class UserShowSerializer(UserSerializer):
+    products = serializers.SlugRelatedField(many=True,queryset=Product.objects.all(),slug_field='title')
     class Meta:
         model = UserProfile
-        fields = ('username','email','first_name','last_name','user_age','user_country','user_city','user_address','is_vendor','user_gender')
+        fields = ('username','email','first_name','last_name','user_age','user_country','user_city','user_address','is_vendor','user_gender','products')
