1fa1bec664800f84c63cee19fe077b8287f07697
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-11 15:34:25 +0300

    product recieved from backend

--- ecommfrontend/src/views/HomeView.vue
+++ ecommfrontend/src/views/HomeView.vue
@@ -3,8 +3,8 @@
     <div class="container-fluid bg-dark">
       <h3 class="text-warning text-center mt-3">New Products</h3>
       <div class="p-5 row d-flex">
-        <div v-for="product in 8" :key="product" class="my-3 col-lg-3 col-md-4 col-sm-6">
-          <Product/>
+        <div v-for="product in products" :key="product.id" class="my-3 col-lg-3 col-md-4 col-sm-6">
+          <Product :product="product"/>
         </div>                 
       </div>
     </div>
@@ -13,13 +13,26 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 import Product from '../components/Product.vue'
 
 export default {
   name: 'HomeView',
-  components: { Product
-   
+  components: { 
+    Product   
+  },
+  data(){
+    return {
+      products:[]
+    }
+  },
+  created(){
+    axios 
+        .get('api/products')
+        .then(response=>{
+          this.products=response.data
+        })
   }
+
 }
 </script>
