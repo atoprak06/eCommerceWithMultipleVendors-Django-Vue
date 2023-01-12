a320aa1324ad0bf537184db56182cfa20d23a7db
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-12 14:59:10 +0300

    router link refactored for name

--- ecommfrontend/src/components/Footer.vue
+++ ecommfrontend/src/components/Footer.vue
@@ -2,7 +2,7 @@
     <div class="footer container text-center">
         <div v-if="tokenStore.isAuthenticated">
             <h3 class="text-warning">Hello {{tokenStore.user.username}}</h3>
-            <router-link to="edit-profile" class="text-decoration-none">Edit your profile</router-link>
+            <router-link :to="{name:'editprofile'}" class="text-decoration-none">Edit your profile</router-link>
         </div>
         <h2 class="text-white">eCommerce 2023</h2>
     </div>    
