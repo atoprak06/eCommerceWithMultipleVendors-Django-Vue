4b708ed99b38b1709a8a354c74b65386acbcee18
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-12 14:56:25 +0300

    slug field modified for blank values

--- ecommbackend/products/models.py
+++ ecommbackend/products/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='products')
     created_by = models.ForeignKey(UserProfile,on_delete=models.CASCADE,related_name='products')
     title = models.CharField(max_length=100)
-    slug = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)    
     price = models.DecimalField(max_digits=8,decimal_places=2)
     description = models.TextField(max_length=255,null=True,blank=True)
