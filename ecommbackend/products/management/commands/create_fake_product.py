8093a2bbdc2333711707a96399f50e6511052875
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-14 14:16:32 +0300

    fake product data creation command added

---
+++ ecommbackend/products/management/commands/create_fake_product.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+import random
+from user.models import UserProfile
+from ...models import Product,Category
+from django.utils.text import slugify
+
+class Command(BaseCommand):
+    help = 'Generate random product data'
+
+    def handle(self, *args, **options):
+        fake = Faker()
+        choices_list = ['active','deactive']     
+        for _ in range(2):
+            title=fake.name(),            
+            Product.objects.create(
+                category=random.choice(Category.objects.all()),
+                created_by=random.choice(UserProfile.objects.all()),
+                title=title,              
+                slug=slugify(title),
+                description=fake.paragraph(),
+                price=fake.random_number(digits=3),
+                product_state=random.choice(choices_list),
+                image_url = fake.image_url()             
+            )
+        self.stdout.write(self.style.SUCCESS('Successfully generated random product data'))
