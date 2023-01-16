c269714c3560ddeeddf50c3a960804cac203a4a2
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-16 19:09:07 +0300

    category page added

--- ecommfrontend/src/views/CategoriesView.vue
+++ ecommfrontend/src/views/CategoriesView.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="categories">
-    <h1 class="text-white">This is categories page</h1>
+  <div class="categories d-flex row p-5">
+    <div v-for="product in products" :key="product.id" class="my-3 col-lg-3 col-md-4 col-sm-6">
+       <Product :product="product"/>
+    </div>    
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+import Product from '../components/Product.vue'
+export default {
+data(){
+  return {
+    products:[], 
+  }
+},
+watch:{  
+  '$route.params.id':{
+    handler: function(id){
+      axios.get(`api/categories/${id}/`)
+        .then(response=>{
+          this.products=response.data.products
+        })
+    },
+    deep:true,
+    immediate:true
+  }
+},
+components:{Product},
+}  
+
+</script>
