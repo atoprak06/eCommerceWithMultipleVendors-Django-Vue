339e0cdad9ae2519d425d143c59714b0f1d255d7
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-17 16:25:20 +0300

    products listed with user is_vendor=True in ProductViewSet,filterby username added to VendorViewSet,active producs on category added to queryset of CategoryViewSet

--- ecommbackend/products/views.py
+++ ecommbackend/products/views.py
@@ -6,10 +6,10 @@ from rest_framework.response import Response
 from .serializers import ProductSerializer,CategorySerializer
 from .models import Product,Category
 from user.models import UserProfile
-from user.serializers import UserShowSerializer
+from user.serializers import UserShowSerializerVendor
 from django.template.defaultfilters import slugify
 from django.db.models import Prefetch
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
@@ -17,14 +17,12 @@ class ProductViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]  
 
     def perform_create(self, serializer):        
-        serializer.save(created_by=self.request.user,slug=slugify(self.request.data['title']))
-        
+        serializer.save(created_by=self.request.user,slug=slugify(self.request.data['title']))        
     
     def list(self,request):
-        products = Product.objects.filter(product_state='active')
+        products = Product.objects.filter(product_state='active',created_by__is_vendor=True)
         serializer= ProductSerializer(products,many=True)
-        return Response(serializer.data)
-       
+        return Response(serializer.data)       
     
     @action(detail=False)
     def my_products(self,request):
@@ -33,13 +31,17 @@ class ProductViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 class VendorsViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = UserShowSerializer
+    serializer_class = UserShowSerializerVendor
     permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
-    
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['username']
+  
     def get_queryset(self):
-        return UserProfile.objects.filter(is_vendor=True) 
+        queryset=UserProfile.objects.prefetch_related(Prefetch('products',queryset=Product.objects.filter(product_state='active'))).filter(is_vendor=True)           
+        username = self.request.query_params.get('username')
+        queryset.filter(username=username)        
+        return queryset
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
-    queryset = Category.objects.prefetch_related(Prefetch('products',queryset=Product.objects.filter(product_state='active')))    
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Category.objects.prefetch_related(Prefetch('products',queryset=Product.objects.filter(product_state='active',created_by__is_vendor=True)))
