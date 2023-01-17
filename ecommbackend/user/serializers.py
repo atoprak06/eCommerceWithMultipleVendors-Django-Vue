1ec62e6757c0b59f691dd033d47d3590626a0367
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-17 16:26:39 +0300

    seperate serializer UserShowSerializerVendor created with products field and plain user model same as before

--- ecommbackend/user/serializers.py
+++ ecommbackend/user/serializers.py
@@ -3,14 +3,24 @@ from djoser.serializers import UserSerializer
 from rest_framework import serializers
 from .models import UserProfile
 from products.models import Product
+from products.serializers import ProductSerializer
 
 class UserRegistrationSerializer(UserCreatePasswordRetypeSerializer):
     class Meta:
         model = UserProfile
         fields = ('username','email','first_name','last_name','user_age','user_country','user_city','user_address','is_vendor','user_gender','password')
 
-class UserShowSerializer(UserSerializer):
-    products = serializers.SlugRelatedField(many=True,queryset=Product.objects.all(),slug_field='title')
+class UserShowSerializerVendor(UserSerializer):
+    products = ProductSerializer(many=True)
     class Meta:
         model = UserProfile
         fields = ('username','email','first_name','last_name','user_age','user_country','user_city','user_address','is_vendor','user_gender','products')
+        read_ony_fields = ['products',]
+
+
+class UserShowSerializerEdit(UserSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('username','email','first_name','last_name','user_age','user_country','user_city','user_address','is_vendor','user_gender',)
+        
+
