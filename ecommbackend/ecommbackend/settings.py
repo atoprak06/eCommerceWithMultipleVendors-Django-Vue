ef0632a040e01fd68c61045ca2cd148cfded09a9
Abdullah Toprak <abdullahtoprakcontact@gmail.com>
2023-01-23 18:37:44 +0300

    pagitanion settings added

--- ecommbackend/ecommbackend/settings.py
+++ ecommbackend/ecommbackend/settings.py
@@ -20,6 +20,8 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 REST_FRAMEWORK = {   
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 16,
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
